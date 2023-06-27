68ded492094c4ae2368c74b77917f6bf20b4ac8c
evgeniya1011 <zhdavydova@mail.ru>
2023-06-27 18:12:58 +0500

    add Phone

---
+++ src/phone.py
@@ -0,0 +1,17 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name, price, quantity, number_of_sim):
+        super().__init__(name, price, quantity)
+        self.__name = name
+        self.number_of_sim = number_of_sim
+        if isinstance(number_of_sim, int) and number_of_sim <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def name(self):
+        return self.__name
