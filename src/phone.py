6f55ecdb3c250d514118f5c30538059ec349700d
evgeniya1011 <zhdavydova@mail.ru>
2023-06-28 01:08:39 +0500

    change flake8

--- src/phone.py
+++ src/phone.py
@@ -5,9 +5,10 @@ class Phone(Item):
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
         self.__name = name
-        self.number_of_sim = number_of_sim
+        self.number_of_sim = int(number_of_sim)
         if isinstance(number_of_sim, int) and number_of_sim <= 0:
             raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity}, {self.number_of_sim})"
