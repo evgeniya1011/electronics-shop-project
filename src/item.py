cd8e6cbb6a92b590d75e893dbe911d9271b7d99f
evgeniya1011 <zhdavydova@mail.ru>
2023-07-01 16:17:36 +0500

    flake8 Item

--- src/item.py
+++ src/item.py
@@ -1,7 +1,8 @@
 import csv
+from abc import ABC
 
 
-class Item:
+class Item(ABC):
     """
     Класс для представления товара в магазине.
     """
@@ -16,6 +17,7 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
+        super().__init__()
         self.__name = name
         self.price = price
         self.quantity = quantity
